a45d21f34833aa1fbaa4a0f1612a80512830c333
lau <gonzzadp@gmail.com>
2020-06-19 09:18:13 -0300

    commit 2

---
+++ app/src/main/java/com/example/appsqllite/ConexionSQLiteHelper.java
@@ -0,0 +1,33 @@
+package com.example.appsqllite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.appsqllite.utilidades.Utilidades;
+
+public class ConexionSQLiteHelper extends SQLiteOpenHelper {
+
+
+
+    public ConexionSQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        //un nuevo cambio de gut
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Utilidades.CREAR_TABLA_USUARIO);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS usuarios");
+        onCreate(db);
+
+    }
+}
